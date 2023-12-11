79300f90fa5edaae2e9fd884fb4e5c3cc6615c8c
amirhossiennamavar <amirhossiennamavar81@gmail.com>
2023-12-11 23:21:43 +0330

    my first try

---
+++ Hw2/Driver.py
@@ -0,0 +1,20 @@
+import sys
+from antlr4 import *
+from ExprLexer import ExprLexer
+from ExprParser import ExprParser
+from VisitorInterp import VisitorInterp
+
+def main(argv):
+    input_stream = FileStream(argv[1])
+    lexer = ExprLexer(input_stream)
+    stream = CommonTokenStream(lexer)
+    parser = ExprParser(stream)
+    tree = parser.start_()
+    if parser.getNumberOfSyntaxErrors() > 0:
+        print("syntax errors")
+    else:
+        vinterp = VisitorInterp()
+        vinterp.visit(tree)
+
+if __name__ == '__main__':
+    main(sys.argv)
